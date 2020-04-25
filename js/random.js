72747665c0e7d11b3c145e420ad45398580a50a3
Hugoren Martinako <aumpfbahn@gmail.com>
2020-04-25 12:35:31 +0200

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,4 +1,6 @@
 /**
  * Creates an alphanumeric random string
+ *
+ * @param {Number} len Length of the seed
  */
-export const seed = () => [...Array(30)].map(() => Math.random().toString(36)[2]).join('');
+export const seed = (len = 24) => [...Array(len)].map(() => Math.random().toString(36)[2]).join('');
