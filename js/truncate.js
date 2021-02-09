63224287fe1ac1a650054063dead60a88a675fa3
Hugoren Martinako <aumpfbahn@gmail.com>
2021-02-09 16:47:57 +0100

    Update truncate.js

--- js/truncate.js
+++ js/truncate.js
@@ -1,4 +1,5 @@
-export const truncate = (str = "", max = 100) =>
+/* trim a large string, as of N-th char, keeping complete words */
+export const truncate = (str = "", max = 100, ellipsis = "\u2026") =>
   str.length > max
-    ? str.substring(0, str.substring(0, max + 1).search(/\s+\S*$/))
+    ? str.substring(0, str.substring(0, max + 1).search(/\s+\S*$/)) + ellipsis
     : str;
