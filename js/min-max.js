ea741d30e8cf5e1ced00964560acba33907de39c
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-16 12:04:50 +0200

    Update min-max.js

--- js/min-max.js
+++ js/min-max.js
@@ -3,4 +3,6 @@
  * @param {Array} data Numeric array
  * @returns {Array} [min, max] 
  */
-export const minMax = data => data.reduce(([ min, max ], item) => ([!min || item < min ? item : min, !max || item > max ? item : max ]), [ null, null ])
+export default function minMax(data) {
+  return data.reduce(([ min, max ], item) => ([!min || item < min ? item : min, !max || item > max ? item : max ]), [ null, null ])
+}
