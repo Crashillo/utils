4348c1be5622667d0ca1796b3844831430db356b
Hugoren Martinako <aumpfbahn@gmail.com>
2022-03-31 11:23:37 +0200

    Update filter-content.js

--- js/filter-content.js
+++ js/filter-content.js
@@ -1,4 +1,16 @@
-const HIDDEN_CSS_CLASS = "hidden";
+/**
+ * Use [data-filter] tag in some parent node relative of the input search and the content to filter
+ * Use [data-filter-target] tag in the nodes to filter
+ *
+ * @example
+ *  <div data-filter>
+ *    <input type="text">
+ *    <div data-filter-target>...</div>
+ *    <div data-filter-target>...</div>
+ *    <div data-filter-target>...</div>
+ *  </div>
+ */
+const HIDDEN_CSS_CLASS = "is-hidden";
 
 function handleTabClick({ target }) {
   const parent = target.closest(`[data-filter]`);
@@ -6,7 +18,7 @@ function handleTabClick({ target }) {
 
   if (parent) {
     [...parent.querySelectorAll(`[data-filter-target]`)].map((x) =>
-      !x.textContent.match(new RegExp(value, "i")) ? x.classList.add(HIDDEN_CLASS) : x.classList.remove(HIDDEN_CLASS)
+      !x.textContent.match(new RegExp(value, "i")) ? x.classList.add(HIDDEN_CSS_CLASS) : x.classList.remove(HIDDEN_CSS_CLASS)
     );
   }
 }
